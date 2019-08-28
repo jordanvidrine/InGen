c8b8ba9794ac88051ef3385debea6145e304257d
jordanvidrine <jordan@jordanvidrine.com>
2019-08-28 15:10:45 -0500

    Script now deletes site and recreates it - created a recursive delete script to clear out site dir - created a script to re-create the site structure

--- buildScripts/buildMultiple.js
+++ buildScripts/buildMultiple.js
@@ -2,24 +2,30 @@ const Mustache = require('mustache')
 const fs = require('fs')
 const fsPromises = fs.promises
 const MarkdownIt = require('markdown-it')();
+const { cleanOut, createSiteDir } = require('./cleanout')
 
 // resets the character escaping to not escape any chars
 // had issues with handlebars escaping < and > tags in html for some reason
 Mustache.escape = function(text) {return text;};
 
 // This script will look at all of the MD files in content
-
-
+// and return an object containing section titles as keys, and their corresponding
+// rendered markdown to html text
 async function getSectionContent () {
   let sectionContent = {};
   try {
     let sections = await fsPromises.readdir('./content/sections');
 
+    // renders markdown into html files and stores them to sectionContent object
     for (let i = 0; i < sections.length; i++) {
-      let rawContent = await fsPromises.readFile(`./content/sections/${sections[i]}`, 'utf8')
-      let renderedContent = MarkdownIt.render(rawContent)
-      let html = '<section>' + renderedContent + '</section>'
-      sectionContent[`${sections[i].split('.')[0]}`] = html;
+
+      // only render files ending in .md
+      if (/(.md)$/.test(sections[i])) {
+        let rawContent = await fsPromises.readFile(`./content/sections/${sections[i]}`, 'utf8')
+        let renderedContent = MarkdownIt.render(rawContent)
+        let html = '<section>' + renderedContent + '</section>'
+        sectionContent[`${sections[i].split('.')[0]}`] = html;
+      }
     }
 
     return sectionContent;
@@ -51,8 +57,6 @@ async function hbsParser(path) {
     }
   })
 
-  console.log(options)
-
   // removing front matter from content
   content = content.replace(/(---\r?\n)(.*\r?\n)*(---\r?\n)/g, "");
 
@@ -70,7 +74,7 @@ async function hbsParser(path) {
     content,
     // save the style to the actual location in the built site directory
     // will be inserted into the generated html as a stylesheet link
-    style: `./css/${options.style}.css`,
+    style: `./assets/css/${options.style}.css`,
     sections: options.sections
   }
 
@@ -80,11 +84,18 @@ async function hbsParser(path) {
   // render the page using the template and the view
   let output = Mustache.render(template, view)
 
+  // clear _site and recreate it
+  if (fs.existsSync('./_test-site')) {
+    cleanOut('./_test-site')
+  }
+
+  await createSiteDir('./_test-site')
+
   // save the rendered output to an html file
   await fsPromises.writeFile(`_test-site/${path.split('/')[path.split('/').length-1].split('.')[0]}.html`,output, 'utf8')
 
   // copy the css stylesheet chosen into the css folder
-  await fsPromises.copyFile(`./templates/css/${options.style}.css`, `_test-site/css/${options.style}.css`)
+  await fsPromises.copyFile(`./templates/css/${options.style}.css`, `_test-site/assets/css/${options.style}.css`)
 }
 
  hbsParser('./content/index.md')
---
+++ buildScripts/cleanout.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const fsPromises = fs.promises
+
+// inspired by https://geedew.com/remove-a-directory-that-is-not-empty-in-nodejs/
+
+let cleanOut = function(path) {
+    let dir = fs.readdirSync(path)
+
+    for (let i = 0; i < dir.length ; i++) {
+      currentPath = `${path}/${dir[i]}`;
+      let stats = fs.lstatSync(currentPath);
+      let isDirectory = stats.isDirectory()
+
+      if (isDirectory) {
+        cleanOut(currentPath)
+      } else {
+        fs.unlinkSync(currentPath)
+        // console.log(`Deleted: ${currentPath}`)
+      }
+    }
+    fs.rmdirSync(path)
+    console.log(`Deleted: ${path}`)
+
+}
+
+let createSiteDir = async function(path) {
+  console.log('here')
+  let css = `${path}/assets/css`.replace(/\/+$/,"")
+  let img = `${path}/assets/img`.replace(/\/+$/,"")
+  let js = `${path}/assets/js`.replace(/\/+$/,"")
+
+  try {
+    await fsPromises.mkdir(css, {recursive:true})
+    await fsPromises.mkdir(img, {recursive:true})
+    await fsPromises.mkdir(js, {recursive:true})
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+module.exports = { cleanOut, createSiteDir }
