c37b6e2c5482da0343b166eeb33b4e7aeb929725
jordanvidrine <jordan@jordanvidrine.com>
2019-09-04 11:26:59 -0500

    Refactoring -Separated getPageContent and getSectionContent -removed unused scripts -consolidated the two content scripts into one file

--- buildScripts/build.js
+++ buildScripts/build.js
@@ -4,9 +4,8 @@ const fsPromises = fs.promises
 const path = require('path')
 const MarkdownIt = require('markdown-it')();
 
-const getData = require('./helpers/getData')
-const getSectionContent = require('./helpers/getSectionContent')
-const { cleanOut, createSiteDir } = require('./helpers/cleanout')
+const { getSectionContent, getPageContent } = require('./helpers/content')
+const { cleanOut, createSiteDir } = require('./helpers/cleaner')
 
 // resets the character escaping to not escape any chars
 // had issues with handlebars escaping < and > tags in html for some reason
@@ -14,21 +13,23 @@ Mustache.escape = function(text) {return text;};
 
 async function build(path) {
   try {
-    let data = await getData(path);
+    let sections = await getSectionContent(path);
+    let page = await getPageContent(path);
+
     // setting view to be the content and any options present from the md file
     // this contains the data that will be injected into the handlebars template
     // used to generate the html
     let view = {
-      title: data.options.title,
-      content: data.pageContent,
+      title: page.options.title,
+      content: page.content,
       // save the style to the actual location in the built site directory
       // will be inserted into the generated html as a stylesheet link
-      style: `./assets/css/${data.options.style}.css`,
-      sections: data.sections
+      style: `./assets/css/${page.options.style}.css`,
+      sections
     }
 
     // get the template content
-    let template = await fsPromises.readFile(data.options.template, 'utf8')
+    let template = await fsPromises.readFile(page.options.template, 'utf8')
 
     // render the page using the template and the view
     let output = Mustache.render(template, view)
@@ -44,7 +45,7 @@ async function build(path) {
     await fsPromises.writeFile(`_test-site/${path.split('/')[path.split('/').length-1].split('.')[0]}.html`,output, 'utf8')
 
     // copy the css stylesheet chosen into the css folder
-    await fsPromises.copyFile(`./templates/css/${data.options.style}.css`, `_test-site/assets/css/${data.options.style}.css`)
+    await fsPromises.copyFile(`./templates/css/${page.options.style}.css`, `_test-site/assets/css/${page.options.style}.css`)
   } catch(e) {
     console.log(e)
   }
--- buildScripts/helpers/content.js
+++ buildScripts/helpers/content.js
@@ -1,11 +1,37 @@
-const fs = require('fs')
-const fsPromises = fs.promises
 const MarkdownIt = require('markdown-it')();
+const fsPromises = require('fs').promises
+
+// This script will look at all of the MD files in /content/sections
+// and return an object containing section titles as keys, and their corresponding
+// rendered markdown to html text
+async function getSectionContent () {
+  let sectionContent = {};
+  try {
+    let sections = await fsPromises.readdir('./content/sections');
+
+    // renders markdown into html files and stores them to sectionContent object
+    for (let i = 0; i < sections.length; i++) {
+
+      // only render files ending in .md
+      if (/(.md)$/.test(sections[i])) {
+        let rawContent = await fsPromises.readFile(`./content/sections/${sections[i]}`, 'utf8')
+        let renderedContent = MarkdownIt.render(rawContent)
+        let html = '<section>' + renderedContent + '</section>'
+        sectionContent[`${sections[i].split('.')[0]}`] = html;
+      }
+    }
+
+    return sectionContent;
+  }
+  catch (e) {
+    return e;
+  }
+}
 
 async function getPageContent(path) {
 
   let content = await fsPromises.readFile(path, 'utf8');
-  // create array from page content to be able to parse options
+  // create array from page content to be able to parse options (strips whitespace)
   contentArray = content.split('\n').map(line => line.replace('\r',''))
 
   let options = {}
@@ -34,4 +60,7 @@ async function getPageContent(path) {
   }
 }
 
-module.exports = getPageContent
+module.exports = {
+  getSectionContent,
+  getPageContent
+}
